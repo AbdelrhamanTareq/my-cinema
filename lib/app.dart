bdc78b8edf4dfa309c15f7056e4e9be44812710e
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2022-12-09 09:45:26 +0200

    change status bar color

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/shared/config/routes_manger.dart';
 import 'package:movies_app/shared/config/theme_manger.dart';
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-     
       builder: (BuildContext context, Widget? child) {
-        return  MaterialApp(
+        SystemChrome.setSystemUIOverlayStyle(
+            const SystemUiOverlayStyle(statusBarColor: Colors.black));
+        return MaterialApp(
           useInheritedMediaQuery: true,
           builder: DevicePreview.appBuilder,
           locale: DevicePreview.locale(context),
@@ -20,12 +22,9 @@ class MyApp extends StatelessWidget {
           theme: ThemeManger.getAppTheme(),
           onGenerateRoute: RouteGenerator.getRoute,
         );
-         
       },
-      
       minTextAdapt: true,
       splitScreenMode: true,
-      
     );
   }
 }
